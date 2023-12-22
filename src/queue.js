afa8cd470dd2eb7c6666563a09091e45ea241f44
makzhentos <makzhentos@gmail.com>
2023-12-22 11:17:50 +0300

    feat: add solution

--- src/queue.js
+++ src/queue.js
@@ -26,8 +26,7 @@ class Queue {
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // return this.arr.shift()
   }
 }
 
